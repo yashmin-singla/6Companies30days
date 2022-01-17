a5e91b77d2970c4141a9e2794eed2e509a943a76
Yashmin Singla <yashminsingla25@gmail.com>
2022-01-17 20:15:58 +0530

    Added solution - LeetHub

---
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxProfit(int k, int n, int a[]) {
+        // code here
+         int dp[k+1][n];
+        
+        for (int i = 0; i <= k; i++)
+            dp[i][0] = 0;
+            
+        for (int j = 0; j < n; j++)
+            dp[0][j] = 0;
+            
+        for(int i=1;i<=k;i++)
+        {
+            int max_profit=INT_MIN;
+            for(int j=1;j<n;j++)
+            {
+                max_profit=max(max_profit,dp[i-1][j-1]-a[j-1]);
+                dp[i][j]=max(dp[i][j-1],max_profit+a[j]);
+            }
+        }
+        return dp[k][n-1];
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> K;
+        cin >> N;
+        int A[N];
+        for (int i = 0; i < N; i++) cin >> A[i];
+
+        Solution ob;
+        cout << ob.maxProfit(K, N, A) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
