85449d0b861919df90cd08761893f04faed9fe58
Yashmin Singla <yashminsingla25@gmail.com>
2022-01-29 13:11:29 +0530

    Added solution - LeetHub

---
+++ Amend The Sentence - GFG/amend-the-sentence.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string amendSentence (string s)
+    {
+        string ans="";
+        int n=s.length();
+        int i=0;
+        while(i<n)
+        {
+            if(i==0 || isupper(s[i]))
+            {
+                ans+=tolower(s[i]);
+                i++;
+            }
+            while(i<n && islower(s[i]))
+            {
+                ans+=s[i];
+                i++;
+            }
+            ans+=" ";
+        }
+        ans.pop_back();
+        return ans;
+        // your code here
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string s; cin >> s;
+		Solution ob;
+		cout << ob.amendSentence (s) << endl;
+	}
+}  // } Driver Code Ends
